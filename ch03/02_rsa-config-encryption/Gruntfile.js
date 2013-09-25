7a2d34cc275b422d25a1f87d1ea619c8d5a024c5
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-25 08:48:54 -0300

    lint, comments

--- ch03/02_rsa-config-encryption/Gruntfile.js
+++ ch03/02_rsa-config-encryption/Gruntfile.js
@@ -1,5 +1,18 @@
 'use strict';
 
+var util = require('util');
+var path = require('path');
+
+// cwd == current working directory: the project root
+var cwd = process.cwd();
+
+// we'll store the private key somewhere safe, like bin/private
+// bin/private should be explicitly ignored in `.gitignore`
+var pemkey = function (name) {
+  var filename = util.format('bin/private/%s.pem', name);
+  return path.join(cwd, filename);
+};
+
 module.exports = function(grunt){
   grunt.initConfig({
     clean: {
@@ -7,16 +20,36 @@ module.exports = function(grunt){
     },
 
     jshint: {
-      me: ['Gruntfile.js']
+      me: ['Gruntfile.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
     },
 
+    // generates the private key
     pem_gen: {
+      // we always use one key for each individual file
+      foo: { pem: pemkey('foo') },
+      bar: { pem: pemkey('bar') }
+    },
 
+    // encrypts the plain file, necessary to update the secure version
+    pem_encrypt: {
+      foo: { pem: pemkey('foo'), store: 'foo' },
+      bar: { pem: pemkey('bar'), store: 'bar' }
+    },
+
+    // decrypts the secure file, making it usable
+    pem_decrypt: {
+      foo: { pem: pemkey('foo'), store: 'foo' },
+      bar: { pem: pemkey('bar'), store: 'bar' }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // load the encryption tasks
   grunt.loadNpmTasks('grunt-pemcrypt');
 };
