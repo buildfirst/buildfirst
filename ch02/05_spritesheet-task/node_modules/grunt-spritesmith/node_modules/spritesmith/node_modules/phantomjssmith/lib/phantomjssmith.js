8aaaa7ca34ccd1b746efc301aba43c24136eabdb
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-10-08 12:39:08 -0300

    removed all node modules folders ever

--- ch02/05_spritesheet-task/node_modules/grunt-spritesmith/node_modules/spritesmith/node_modules/phantomjssmith/lib/phantomjssmith.js
+++
@@ -1,6 +0,0 @@
-// Load in and combine all engine parts
-var extend = require('obj-extend');
-module.exports = extend({},
-  require('./image'),
-  require('./exporters'),
-  require('./canvas'));
--- ch02/05_spritesheet-task/node_modules/grunt-spritesmith/node_modules/spritesmith/node_modules/phantomjssmith/test/phantomjssmith_test_outline.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'phantomjssmith': require('spritesmith-engine-test').outline
-};
