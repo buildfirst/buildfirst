ab9a46838b8e5bc9416726a105aa1f0176583276
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-11-30 00:39:40 -0300

    srp sample

--- ch05/01_single-responsibility-principle/sluggish.js
+++ ch05/01_single-responsibility-principle/sluggish.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var sluggish = require('./sluggish.js');
+var sluggish = require('./sluggish-module.js');
 var article = {
   date: new Date('2013-11-29T20:42:13.922Z'),
   title: '... Foo Some   Poorly Worded----   -  Text-?'
--- ch05/foo_modules/README.md
+++
@@ -1,3 +0,0 @@
-# Using Common.JS Modules
-
-...
