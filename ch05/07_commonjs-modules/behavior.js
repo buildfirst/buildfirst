2ce252a690590ea5252bd8f589c48ea9b0b38f40
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-12-09 00:45:04 -0200

    Create behavior.js

---
+++ ch05/07_commonjs-modules/behavior.js
@@ -0,0 +1,16 @@
+
+// variables declared on the top-most scope won't become globals
+var something = {
+  foo: 'bar'
+};
+
+// instead, globals need to be explicitly declared in Common.JS modules
+global.thing = something;
+
+// our module's public interface is `module.exports`
+module.exports = {
+  bar: 'baz'
+};
+
+// thus, when our module is required, its contents will be executed, and
+// then we get access to only the public interface
