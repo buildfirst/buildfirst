b0b31ea1630801d93baf2bdf5faeb2ae82b93f01
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-12-09 00:49:14 -0200

    Update behavior.js

--- ch05/07_commonjs-modules/behavior.js
+++ ch05/07_commonjs-modules/behavior.js
@@ -1,4 +1,3 @@
-
 // variables declared on the top-most scope won't become globals
 var something = {
   foo: 'bar'
@@ -14,3 +13,10 @@ module.exports = {
 
 // thus, when our module is required, its contents will be executed, and
 // then we get access to only the public interface
+
+
+// modules have access to global variables such as __dirname
+console.log(__dirname);
+// <- this file's directory name
+
+// see http://nodejs.org/api/globals.html#globals_dirname for a full list of globals
