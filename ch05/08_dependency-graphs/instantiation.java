2ee42ce54d5907fe3dafa93c5d59e27aba45b1a4
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-12-10 05:07:22 -0200

    Update instantiation.java

--- ch05/08_dependency-graphs/instantiation.java
+++ ch05/08_dependency-graphs/instantiation.java
@@ -1,5 +1,9 @@
-String connectionString = 'SOME_CONNECTION_STRING';
-SqlConnectionString connString = new SqlConnectionString(connectionString);
-SqlDbConnection conn = new SqlDbConnection(connString);
-IUserRepository repo = new UserMySqlRepository(conn);
-UserService service = new UserService(repo);
+public static class Main {
+    public Main () {
+        String connectionString = 'SOME_CONNECTION_STRING';
+        SqlConnectionString connString = new SqlConnectionString(connectionString);
+        SqlDbConnection conn = new SqlDbConnection(connString);
+        IUserRepository repo = new UserMySqlRepository(conn);
+        UserService service = new UserService(repo);
+    }
+}
