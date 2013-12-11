2754fe6b9bd44dc20f3f2469d141ef4814f921ec
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-12-11 18:50:25 -0300

    commentary in ch05e08

--- ch05/08_dependency-graphs/instantiation.java
+++ ch05/08_dependency-graphs/instantiation.java
@@ -1,3 +1,5 @@
+// This is how painful instancing a service might be if we had to resolve
+// an entire dependency graph by ourselves, instead of relying on a DI framework
 public class Main {
     public static void main (String[] args) {
         String connectionString = "SOME_CONNECTION_STRING";
--- ch05/08_dependency-graphs/userService.java
+++ ch05/08_dependency-graphs/userService.java
@@ -1,6 +1,11 @@
 public class UserService {
     private IUserRepository _userRepository;
 
+    // In this case, our dependency is hard-coded, so
+    // we won't be able to inject a fake implementation
+    // effectively becoming unable to test the UserService class
+    // as a unit, without testing UserMySqlRepository as a side-effect
+    // as well. In testing, side-effects are poison.
     public UserService () {
         _userRepository = new UserMySqlRepository();
     }
