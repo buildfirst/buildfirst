882c8c3f0f14105444a13bd3833ac09499dfef0a
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-12-23 00:26:18 -0300

    added modules which compile to es3 with traceur

---
+++ ch05/17_harmony-traceur/js/modules.js
@@ -0,0 +1,23 @@
+module chickenCoup {
+  var chickens = [];
+
+  export function hatch () {
+    console.log('An egg is hatching!');
+    chickens.push({
+      eggs: 3
+    });
+  };
+
+  export function getChicken (id) {
+    return chickens[id];
+  };
+};
+
+import * from chickenCoup;
+
+hatch();
+
+var chicken = getChicken(0);
+
+console.log(chicken.eggs);
+// <- 3
