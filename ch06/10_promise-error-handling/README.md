cbfe2f83af104e16e1c6d7e2d42b7981bdf57e02
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-03-01 07:22:17 -0500

    example error handle

---
+++ ch06/10_promise-error-handling/README.md
@@ -0,0 +1,3 @@
+# Error Handling Promises
+
+Error handling is promises is as easy as registering a `.catch` callback.
---
+++ ch06/10_promise-error-handling/example.js
@@ -0,0 +1,21 @@
+function delay (t) {
+  function wait (resolve, reject) {
+    if (t < 1) {
+      throw new Error('Delay must be greater than zero.');
+    }
+    setTimeout(function () {
+      console.log('Resolving after', t);
+      resolve(t);
+    }, t);
+  }
+  return new Promise(wait);
+}
+
+Promise
+  .all([delay(0), delay(400)])
+  .then(function (result) {
+    throw new Error('I dislike the result!');
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
