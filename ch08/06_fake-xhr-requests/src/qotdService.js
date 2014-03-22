a44a8624c202a2c82edcd7b84a1939203966f89b
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-03-21 21:05:59 -0300

    Rm comment to set ua

--- ch08/06_fake-xhr-requests/src/qotdService.js
+++ ch08/06_fake-xhr-requests/src/qotdService.js
@@ -3,7 +3,6 @@ var request = require('superagent');
 module.exports = function (done) {
   request
     .get('https://api.github.com/zen')
-    //.set('User-Agent', 'superagent')
     .end(cb);
 
   function cb (err, res) {
