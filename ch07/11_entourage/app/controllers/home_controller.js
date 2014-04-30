0aff18b2afefa77e411e4975b5f442468345a257
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-04-29 23:56:40 -0300

    foo

---
+++ ch07/11_entourage/app/controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports = {
+  index: function (params, cb) {
+    this.redirectTo('/items');
+  }
+};
--- ch07/11_entourage/app/routes.js
+++ ch07/11_entourage/app/routes.js
@@ -1,5 +1,5 @@
 module.exports = function (match) {
-  match('', 'root');
+  match('', 'home#index');
   match('items', 'items#index');
   match('items/add', 'items#add');
 };
