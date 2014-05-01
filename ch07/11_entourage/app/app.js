5e6d3a9ca2cc7731c2f5a848d28b6e1ddb9928c5
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-01 20:41:02 -0300

    cleaning up

--- ch07/11_entourage/app/app.js
+++ ch07/11_entourage/app/app.js
@@ -1,29 +1,4 @@
 var BaseApp = require('rendr/shared/app');
-var handlebarsHelpers = require('./lib/handlebarsHelpers');
 
-/**
- * Extend the `BaseApp` class, adding any custom methods or overrides.
- */
 module.exports = BaseApp.extend({
-
-  /**
-   * Client and server.
-   *
-   * `initialize` is called on app initialization, both on the client and server.
-   * On the server, an app is instantiated once for each request, and in the
-   * client, it's instantiated once on page load.
-   *
-   * This is a good place to initialize any code that needs to be available to
-   * app on both client and server.
-   */
-  initialize: function () {
-    /**
-     * Register our Handlebars helpers.
-     *
-     * `this.templateAdapter` is, by default, the `rendr-handlebars` module.
-     * It has a `registerHelpers` method, which allows us to register helper
-     * modules that can be used on both client & server.
-     */
-    this.templateAdapter.registerHelpers(handlebarsHelpers);
-  }
 });
