b027d31f37254c10bcb427af92a6eced62821844
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-03 13:38:37 -0300

    foo

--- ch07/11_entourage/app/router.js
+++ ch07/11_entourage/app/router.js
@@ -1,11 +1,9 @@
 var BaseClientRouter = require('rendr/client/router');
 
-var Router = module.exports = function Router(options) {
+var Router = module.exports = function Router (options) {
   BaseClientRouter.call(this, options);
 };
 
-/**
- * Set up inheritance.
- */
+// set up inheritance
 Router.prototype = Object.create(BaseClientRouter.prototype);
 Router.prototype.constructor = BaseClientRouter;
