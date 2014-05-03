40deacd1aa2ed37ff276bb1977e08d81b592085f
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-03 14:43:12 -0300

    update base

--- ch07/12_full-metal-jacket/app/views/base.js
+++ ch07/12_full-metal-jacket/app/views/base.js
@@ -1,5 +1,3 @@
-var RendrView = require('rendr/shared/base/view');
+var RendrBase = require('rendr/shared/base/view');
 
-// Create a base view, for adding common extensions to our
-// application's views.
-module.exports = RendrView.extend({});
+module.exports = RendrBase.extend({});
